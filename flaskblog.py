901106149acc95623cfc8a806865ffac720fe5ba
femi <femi1602@gmail.com>
2021-05-11 23:17:40 +0530

    initial commit

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,30 @@
-from flask import Flask
-app= Flask(__name__)
+from flask import Flask, render_template, url_for
+app= Flask(__name__,template_folder='template')
+
+post=[
+    {
+        'author':'Amish Tripathi',
+        'title':'Blog Post 1',
+        'content':'First post content',
+        'date_posted':'2018 April 10'
+    },
+    {
+        'author':'Jane Doe',
+        'title':'Blog Post 2',
+        'content':'Second post content',
+        'date_posted':'2018 April 13'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
-def hello():
-    return "<h1>Home Page</h1>"
+def home():
+    return render_template('home.html',post=post)
+
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html',title='About Page title')
 
 
 if __name__=='__main__':
